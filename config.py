00ea90487bdc5fe8871087abc8f5c75c52cf4f5d
Galxie_S9 <100624075+kanooob@users.noreply.github.com>
2026-02-15 11:34:56 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# Configuration du Bot Stoat
+WELCOME_CHANNEL_ID = "TON_ID_SALON"
+WELCOME_MESSAGE = "Bienvenu {user} on est {count} sur le serveur"
+AUTO_ROLES = ["ID_ROLE1", "ID_ROLE2"] # Liste des IDs de rôles à donner
