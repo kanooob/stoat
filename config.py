6131b6140cd5a0e1b97c20dd807e933204dff71c
Galxie_S9 <100624075+kanooob@users.noreply.github.com>
2026-02-15 11:44:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Configuration du Bot Stoat
-WELCOME_CHANNEL_ID = "TON_ID_SALON"
+WELCOME_CHANNEL_ID = "01KH6VN8ENTS29CWZN00XPCA4Z"
 WELCOME_MESSAGE = "Bienvenu {user} on est {count} sur le serveur"
-AUTO_ROLES = ["ID_ROLE1", "ID_ROLE2"] # Liste des IDs de rôles à donner
+AUTO_ROLES = ["01KH70CX7YBNHJ4PG0EWR7DRXZ", "01KHGEE0HYP14WNSR7V4S6FPFV"] # Liste des IDs de rôles à donner
