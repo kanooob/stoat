07f604ee9934bac44b4a9bba7b3d7fb9f5a5f51e
Galxie_S9 <100624075+kanooob@users.noreply.github.com>
2026-02-18 13:34:57 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 WELCOME_CHANNEL_ID = "01KH6VN8ENTS29CWZN00XPCA4Z"
 WELCOME_MESSAGE = "Bienvenu {user} on est {count} sur le serveur"
 AUTO_ROLES = ["01KH70CX7YBNHJ4PG0EWR7DRXZ", "01KHGEE0HYP14WNSR7V4S6FPFV"] # Liste des IDs de rôles à donner
-# --- Configuration Starboard ---
+SYSTEM_LOGS_ID = "01KHRBXM12X7CDT5P259PA17JH"
 STARBOARD_CHANNEL_ID = "01KHCD1Q4JB7JZ0262ND7CY4JN"
 STARBOARD_LIMIT = 3  # Nombre de réactions nécessaires
 STAR_EMOJI = "⭐"      # L'emoji à surveiller
